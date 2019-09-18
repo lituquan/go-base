05e833d2ca1da31106595ba229b14708c5185d2d
tuquan <1242441055@qq.com>
2019-09-18 14:35:20 +0800

    待实现调用后输入

--- cmd-call/cmd.go
+++ cmd-call/cmd.go
@@ -12,20 +12,11 @@ import (
 	"os/exec"
 	"strings"
 )
-
-func init()  {
-	log.SetFlags(log.Lshortfile)
-}
+//怎么在发起调用时候进行输入
 func start(cmdstr string) {
 	//命令构造
 	cmds := strings.Split(cmdstr, " ")
 	cmd := exec.Command(cmds[0], cmds[1:]...)
-
-	stdin,err:=cmd.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stdin.Close()
 
 	//获取输出对象，可以从该对象中读取输出结果
 	stdout, err := cmd.StdoutPipe()
@@ -36,14 +27,6 @@ func start(cmdstr string) {
 	// 执行命令
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
-	}
-	// 读取输入结果
-	_,err=stdin.Write([]byte("hello"))
-	if  err != nil {
-		log.Fatal(err)
-	}else{
-		ioutil.ReadAll(stdin)
-		log.Println("",)
 	}
 	// 读取输出结果
 	if opBytes, err := ioutil.ReadAll(stdout); err != nil {
